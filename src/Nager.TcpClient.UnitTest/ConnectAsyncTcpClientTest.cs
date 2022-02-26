b2b0443a6dcb1fa7d51bdf7f6993079548e0dcb0
Tino Hager <tino.hager@nager.at>
2022-02-26 21:05:54 +0100

    Optimize UnitTests

--- src/Nager.TcpClient.UnitTest/ConnectAsyncTcpClientTest.cs
+++ src/Nager.TcpClient.UnitTest/ConnectAsyncTcpClientTest.cs
@@ -1,13 +1,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Nager.TcpClient.UnitTest
 {
     [TestClass]
-    public class ConnectTcpClientTest
+    public class ConnectAsyncTcpClientTest
     {
         [TestMethod]
-        public void Connect_WebTestService_Successful()
+        public async Task ConnectAsync_WebTestService_Successful()
         {
             var ipAddress = "tcpbin.com";
             var port = 4242;
@@ -15,32 +15,25 @@ namespace Nager.TcpClient.UnitTest
             var mockLoggerTcpClient = LoggerHelper.GetLogger<TcpClient>();
 
             using var tcpClient = new TcpClient(logger: mockLoggerTcpClient.Object);
-            var connectSuccessful = tcpClient.Connect(ipAddress, port);
+            var connectSuccessful = await tcpClient.ConnectAsync(ipAddress, port);
 
             Assert.IsTrue(connectSuccessful, "Cannot connect");
             Assert.IsTrue(tcpClient.IsConnected, "IsConnected has wrong state");
         }
 
         [TestMethod]
-        public void Connect_NonRoutableIpAddress_Failure()
+        public async Task ConnectAsync_NonRoutableIpAddress_Failure()
         {
             var ipAddress = "10.255.255.1";
             var port = 4242;
-            var connectionTimeoutMilliseconds = 3000;
 
             var mockLoggerTcpClient = LoggerHelper.GetLogger<TcpClient>();
 
             using var tcpClient = new TcpClient(logger: mockLoggerTcpClient.Object);
+            var connectSuccessful = await tcpClient.ConnectAsync(ipAddress, port);
 
-            var sw = new Stopwatch();
-            sw.Start();
-            var connectSuccessful = tcpClient.Connect(ipAddress, port, connectionTimeoutMilliseconds);
-            sw.Stop();
-
-            var allowedTolerance = 100; //100ms
-            Assert.IsTrue(sw.Elapsed.TotalMilliseconds > (connectionTimeoutMilliseconds - allowedTolerance), $"Abort to early {sw.Elapsed.TotalMilliseconds}");
             Assert.IsFalse(connectSuccessful, "Connection should not be possible");
-            Assert.IsFalse(tcpClient.IsConnected);
+            Assert.IsFalse(tcpClient.IsConnected, "IsConnected has wrong state");
         }
     }
 }
